2137b57cdffb068f13892ef93105003ee3a748f2
Laurie Chan Lam <laurie@lady-pony.com>
2019-02-21 16:30:45 +1300

    implement create_directory

--- mudbox2mari.py
+++ mudbox2mari.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
+import os
 
 def create_directory():
-	return 'rename'
+	directory = 'rename'
+	n = 1
+	while os.path.exists(directory):
+		directory = 'rename_' + str(n).zfill(2)
+		n = n + 1
+	os.makedirs(directory) # create the directory
+	print('created directory ' + directory) # show information to user
+	return directory
 
 def get_mudbox_files_in_folder():
 	return ['u0_v0.tif', 'u0_v1.exr', 'u1_v0.tiff']
